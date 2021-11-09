22f93fffac999128e0dd838ff3a2cd488b51d61d
RitikSVala <ri_sangee@hotmail.com>
2021-11-09 21:01:07 +0000

    This is my second commit

--- devops_project.py
+++ devops_project.py
@@ -1,7 +1,7 @@
 from flask import Flask , render_template
 app = Flask(__name__) ##Module Name = __name__ = __main__
 
-
+##Temporary Data Set
 uploads = [
         {
             "user": "RITIK VALA",
