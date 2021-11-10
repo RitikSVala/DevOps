69704d8389c82906223b118de255e0289f3b9942
RitikSVala <ri_sangee@hotmail.com>
2021-11-10 13:06:32 +0000

    Commit with the database created

--- devops_project.py
+++ devops_project.py
@@ -13,29 +13,33 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///devops.db"
 
 db = SQLAlchemy(app)
 
-class User(db.Profile):
+class User(db.Model):
     ##Unique number to define different users also the primary key ("primary_key=True")
-    id = db.column(db.int, primary_key=True)
-    ##String with a max char value of "20", user_names need to be unique so pass in "unique=true", 
-    user_name = db.column(db.string(20), unique=True, nullable=False)
-    ##String with a max char value of "20", user_names need to be unique so pass in "unique=true", 
-    user_email = db.column(db.string(100), unique=True, nullable=False)
-    ##String with a max char value of "50", 
-    uesr_password = db.column(db.string(50), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    ##String with a max char value of "20", user_names need to be unique so pass in "unique=true"
+    user_name = db.Column(db.String(20), unique=True, nullable=False)
+    ##String with a max char value of "20", user_names need to be unique so pass in "unique=true"
+    user_email = db.Column(db.String(100), unique=True, nullable=False)
+    ##String with a max char value of "50"
+    uesr_password = db.Column(db.String(50), nullable=False)
+    ##uploads attribute is related to the upload model
+    uploads= db.relationship("Upload", backref="creator", lazy=True)
 
     ##Defines how the object is printed
     def __repr__(self):
         return f"User: ('{self.user_name}', '{self.user_email}')"
 
 ##Upload class that will hold the posts made onto webpage
-class Upload(db.Profile):
+class Upload(db.Model):
     ##Unique number to define different users also the primary key ("primary_key=True")
-    id = db.column(db.int, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     ##Header for each post can only be 120 char long and must have data inputed.
-    header = db.column(db.string(120), nullable = False)
+    header = db.Column(db.String(120), nullable = False)
     ##Date and time of when the post was made, saves whatever the current time on system is as the date and time
-    date_uploaded = db.column(db.DateTime, nullable = False, default = datetime.utcnow)
-    caption = db.column(db.Text, nullable = False)
+    date_uploaded = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    caption = db.Column(db.Text, nullable = False)
+    ##This will be the ID of the user (creator) of the post
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"),nullable = False)
 
     ##Defines how the object is printed
     def __repr__(self):
