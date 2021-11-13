e92efa5dee3f687e60b01929b36c97e0e3e6cf31
RitikSVala <ri_sangee@hotmail.com>
2021-11-13 12:50:22 +0000

    File structure reloaded

--- devops_project/__init__.py
+++ devops_project/__init__.py
@@ -2,9 +2,10 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from os import getenv
 from flask_bcrypt import Bcrypt
 
-
+##Flask instance!
 app = Flask(__name__) ##Module Name = __name__ = __main__
 ##Secret Key to protect against modifying cookies etc. (Set secret key for the appication)
 app.config["SECRET_KEY"] = "d7545bde35264afcc836e153c2deabce"
