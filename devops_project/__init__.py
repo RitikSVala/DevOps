7d56192cdafb197a66e4bb4a2ddefa7a3682abca
RitikSVala <ri_sangee@hotmail.com>
2021-11-14 17:07:38 +0000

    Commit with file changes to __init__

--- devops_project/__init__.py
+++ devops_project/__init__.py
@@ -10,7 +10,7 @@ app = Flask(__name__) ##Module Name = __name__ = __main__
 ##Secret Key to protect against modifying cookies etc. (Set secret key for the appication)
 app.config["SECRET_KEY"] = "d7545bde35264afcc836e153c2deabce"
 ##SQLALCHEMY - Set path
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv('db_uri')
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///devops.db"
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 bcrypt = Bcrypt(app)
